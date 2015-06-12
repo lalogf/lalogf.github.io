34f6786b8d059c7400471668a65176da851ebddf
lalogf <lalogf@gmail.com>
2015-06-11 22:29:02 -0500

    Added some changes to the css file for better mobile views

--- app.js
+++ app.js
@@ -52,6 +52,10 @@ var ready = function () {
 
 	hideControls();
 
+	$('.nav a').on('click', function(){
+    $(".btn-navbar").click(); //bootstrap 2.x
+    $(".navbar-toggle").click() //bootstrap 3.x by Richard
+});
 
 };
 
