04bb2a91c5f65f496f3bf44f117f894228a3604a
lalogf <lalogf@gmail.com>
2015-06-13 14:01:23 -0500

    Extras section added and Ajax calls fixed

--- server.js
+++ server.js
@@ -163,6 +163,7 @@ function wiwo () {
 	});
 	$("#container").html(content);
 	$("#wiwoButton").css("background-color", "yellow");
+	departamentos();
 }
 
 function blog () {
@@ -181,7 +182,61 @@ function contact () {
 	$("#contactButton").css("background-color", "yellow");
 }
 
+var departamentos = function (){
+	var depo, depid;
 
+	$.ajax({
+		url: "https://powerful-hollows-4606.herokuapp.com/departamentos.json",
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		type: "GET",
+		success: function(data) {
+			$("#departamentos").empty();
+			depo = data;
+			$(data).each(function (i) {
+				$("#departamentos").append("<option value=\""+data[i].departamento +"\" number=\""+data[i].id+"\" id=\""+i+"\" >"+data[i].departamento+"</option>");
+			});
+			$("#provincias").empty();
+			$(data[0].provincias).each(function(i){
+				$("#provincias").append("<option value= \""+ data[0].provincias[i].provincia.nombre + "\" number = \"" + data[0].provincias[i].provincia.id + "\" id=\""+i+"\"  >" + data[0].provincias[i].provincia.nombre + "</option>");
+			});
+			$("#distritos").empty();
+			$(data[0].provincias[0].distritos).each(function(i){
+				$("#distritos").append("<option value= \""+ data[0].provincias[0].distritos[i].nombre + "\" number = \"" + data[0].provincias[0].distritos[i].id + "\" id=\""+i+"\" >" + data[0].provincias[0].distritos[i].nombre + "</option>");
+				
+			});	
+
+		},
+		error: function() {
+			alert("something went wrong ..."); 
+		}
+	});
+
+	$("#departamentos").change(function(){
+		depid = $("#departamentos").find(':selected').attr('id');
+		var provs = depo[depid].provincias;
+		var distritos_op1 = provs[0].distritos
+		$("#provincias").empty();
+		$(provs).each(function(i){
+				$("#provincias").append("<option value= \""+ provs[i].provincia.nombre + "\" number = \"" + provs[i].provincia.id + "\" id=\""+ i +"\" >" + provs[i].provincia.nombre + "</option>");
+			});
+			$("#distritos").empty();
+			$(distritos_op1).each(function(i){
+				$("#distritos").append("<option value= \""+ distritos_op1[i].nombre + "\">" + distritos_op1[i].nombre + "</option>");
+			});
+	});
+
+	$("#provincias").change(function(){
+		var provid = $("#provincias").find(':selected').attr('id');
+		var distritos_provincia_selected = depo[depid].provincias[provid].distritos;
+		$("#distritos").empty();
+		$(distritos_provincia_selected).each(function(i){
+			$("#distritos").append("<option value= \""+ distritos_provincia_selected[i].nombre + "\">" + distritos_provincia_selected[i].nombre +   "</option>");
+		});
+	});
+}
 
 
 
