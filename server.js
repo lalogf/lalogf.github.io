000db06eb1eeb1edf16b855844cc2ca293483c1b
lalogf <lalogf@gmail.com>
2016-06-09 17:20:39 -0500

    some updates

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ function index () {
 	var content = indexTemplate({
 		name: "LALO GONZALEZ",
 		title1: "WEB DEVELOPER",
-		title2: "MARKETING"
+		title2: "UX DESIGNER"
 	});	
 	$("#container").html(content);
 };
@@ -66,16 +66,14 @@ function index () {
 function about (){
 	var content = aboutTemplate({
 		title: "About",
-		intro: "I'm a Web Developer and Marketer, who enjoys building usable and enjoyable stuff for the Internet age.",
+		intro: "I'm a UX Designer, Marketer and Web Developer, who enjoys building usable and enjoyable stuff for the Internet age.",
 		items: 
-		[{ 	title: "Web Developer",
-		text: "I can code in Ruby, Javascript, HTML and CSS. I experienced working with frameworks such as: Ruby on Rails, Nodejs, Sequelize, Express, Handlebars and Backbone. I normally use Postgresql as DB and Passport/Bcrypt or Devise for authentication. I have deployed to Heroku and AWS. I was trained at General Assembly in San Francisco, CA and I'm sharping my skills at Onemonth.com."
-	},
-	{  title: "Marketing",
-	text: "I have more than 5 years of experience in Marketing. I have participated in the planning and design of the most downloaded and used Mobile Banking app in Peru. I have helped a NGO that defends Human Rigths in the Digital field. I had a couple of entrepreneurial adventures. I'm certified in Digital Marketing and I'm currently attending a Growth Hacking online course.",
-}
-]
-});	
+		[{  title: "UX Designer - Marketing",
+			text: "I have about 8 years of experience in Product Managment and UX Design. I have participated in the planning and design of the most downloaded and used Mobile Banking app in Peru. I have helped a NGO that defends Human Rigths in the Digital field. I had a couple of entrepreneurial adventures. I'm certified in Digital Marketing and I have taken General Assembly's UX Design online course."},
+			{ 	title: "Web Developer",
+		text: "I can code in Ruby, Javascript, HTML and CSS. I experienced working with frameworks such as: Ruby on Rails, Nodejs, Sequelize, Express, Handlebars and Backbone. I normally use Postgresql as DB and Passport/Bcrypt or Devise for authentication. I have deployed to Heroku and AWS. I was trained at General Assembly in San Francisco, CA and I'm sharping my skills at Onemonth.com."}
+		]
+		});	
 	$("#container").html(content);
 	$("#aboutButton").css("background-color", "yellow");
 };
@@ -142,11 +140,11 @@ function wiwo () {
 			modalId: "github"
 		},
 		{
-			title: "Soulutions",
+			title: "Arte Aparte",
 			text: "Website for a new Digital Boutique. Showing their previous works and offering their services to create and develop astonishing digital products.",
-			link: "data-toggle=modal data-target=#screenshotModal",
-			button: "Screenshot",
-			modal: true,
+			link: "href=https://arteaparte.herokuapp.com/crea-tu-case/new target=blank",
+			button: "Live App - Dev Mode",
+			modal: false,
 			modalId: "screenshotModal",
 			modalcontent:"<img style=width:100% src=http://www.ucarecdn.com/21d579b3-01c8-4548-a01c-bb11537affb2/ScreenShot20150613at125002AM.png>"
 		},
